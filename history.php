77db5fbdafe5f4e8c18367baa267cbaa6780cbf3
sahal alzubair <inzilagiway@gmail.com>
2019-11-08 18:41:53 +0300

    added jquery locally

--- history.php
+++ history.php
@@ -9,7 +9,7 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="style/bootstrap.min.css">
 	<link rel="stylesheet" href="style/main.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="javascript/jquery-3.2.1.min.js"></script>
 	<script src ="javascript/bootstrap.min.js"></script>
 	<title>مطبعة الليزر</title>
 </head>
